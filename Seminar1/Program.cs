2a348217279360d2cf6d090e7509a1e5fbf3a2f4
Valentin Burov <burov0742@gmail.com>
2022-06-06 11:41:51 +0300

    First seminar completed

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+int num;
+
+Console.Write("Imput intereg number : ");
+num = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Result is " +  num*num);
+*/
+
+/*
+
+Задача на сравнение. Является ли первое число квадратом второго.
+
+int num1, num2, quad;
+
+Console.WriteLine("Imput first intereg number :");
+num1 = Convert.ToInt32 (Console.ReadLine());
+
+Console.Write("Imput second intereg number : ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+quad = num2 * num2;
+
+if(num1 == quad)
+{
+    Console.WriteLine("true");
+}
+else
+{
+    Console.WriteLine("false");
+}
+*/
+
+/*
+
+Задачан на запись целочисленного числа от отрицательного его значние до положительного.
+
+int num;
+
+Console.Write("Imput intereg number :");
+num = Convert.ToInt32 (Console.ReadLine());
+
+int current = num * (-1);
+
+while(current <= num)
+{
+    Console.Write(current + " ");
+    current ++; //* Тоже самое, что current = current + 1;
+}
+*/
+
