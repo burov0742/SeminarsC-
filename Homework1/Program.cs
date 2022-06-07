b6078ce6dc96a801b86f6351da0e8f5f07aa0775
Valentin Burov <burov0742@gmail.com>
2022-06-07 11:01:37 +0300

    Homework1 added

---
+++ Homework1/Program.cs
@@ -0,0 +1,16 @@
+﻿int num1;
+int num2;
+Console.WriteLine("Введите первое число: ");
+num1 = Console.ReadLine();
+
+Console.WriteLine("Ввкдите второе число: ");
+num2 = Console.ReadLine();
+
+if (num1>num2)
+{
+Console.WriteLine("Первое число больше втрого");
+}
+else 
+{
+    Console.WriteLine("Втрое число больше первого");
+}
