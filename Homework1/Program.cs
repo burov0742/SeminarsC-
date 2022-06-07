0e2e12baae682dca0c8d7a6ad94dd09f7d5e8b5c
Valentin Burov <burov0742@gmail.com>
2022-06-07 13:35:54 +0300

    First, second and third  exercise done

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -1,16 +1,93 @@
-﻿int num1;
-int num2;
-Console.WriteLine("Введите первое число: ");
-num1 = Console.ReadLine();
+﻿
+// Задача: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+/*
 
-Console.WriteLine("Ввкдите второе число: ");
-num2 = Console.ReadLine();
+int num1, num2;
+
+Console.WriteLine("Введите первое число: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+num2 = Convert.ToInt32(Console.ReadLine());
 
 if (num1>num2)
 {
-Console.WriteLine("Первое число больше втрого");
+Console.WriteLine("Первое число больше второго");
 }
 else 
 {
-    Console.WriteLine("Втрое число больше первого");
+    Console.WriteLine("Второе число больше первого");
+
 }
+
+*/
+
+//Напишите программу, которая принимает на ход три числа и выдаёт максимальное из этих чисел.
+
+/*
+
+int num1, num2, num3;
+
+Console.Write("Введите первое число:");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число:");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число:");
+num3 = Convert.ToInt32(Console.ReadLine());
+
+
+if (num1>num2)
+{
+    if (num1>num3)
+    {
+        if (num2<num3)
+        {
+            Console.WriteLine("Число " + num1 + " самое большое, а число " + num2 + " самое меньшее");
+        }
+        else
+        {
+            Console.WriteLine("Число " + num1 + " самое большое, а число " + num3 + " самое меньшее");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Число " + num3 + " самое большое, а число " + num2 + " самое меньшее");
+    }
+}
+
+if (num2>num3)
+{
+    if (num1<num3)
+    {
+        Console.WriteLine("Число " + num2 + " самое большое, а число " + num1 + " самое меньшее");
+    }
+    else
+    {
+        Console.WriteLine("Число " + num2 + " самое большое, а число " + num3 + " самое меньшее");
+    }
+}
+
+// !!!Программа не выдает ошибку и не выдает результат при вводе отрицательного числа первым. Во всех остальных случаях работает!!!
+*/
+
+/*
+// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+int num1;
+
+Console.Write("Введите число: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 % 2 == 0)
+{
+    Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число нечетное");
+}
+
+*/
+
