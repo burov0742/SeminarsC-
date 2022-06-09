f7b952d4554c9dbf1ddab0850938fef70aa7c8cc
Valentin Burov <burov0742@gmail.com>
2022-06-09 10:36:45 +0300

    function adde

--- Seminar2/Program.cs
+++ Seminar2/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿int FindMaxPart(int num)
+
+{
+    int des, ed;
+
+    if (num >= 10 && num <=99)
+    {
+        des = num / 10;
+        ed = num % 10;
+
+        if (des>ed)
+        {
+            return des;
+        }
+        else
+        {
+            return ed;
+        }
+    }
+    else
+    {
+        return -1;
+    }
+}
