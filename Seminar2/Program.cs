7a0ced1c0fc269d07d33fa7f9e2978309b70ad76
Valentin Burov <burov0742@gmail.com>
2022-06-09 12:09:10 +0300

    Seminar2 is ended

--- Seminar2/Program.cs
+++ Seminar2/Program.cs
@@ -1,4 +1,5 @@
-﻿int FindMaxPart(int num)
+﻿/*
+int FindMaxPart(int num)
 {
     int des, ed;
 
@@ -37,3 +38,67 @@ else
     Console.WriteLine($"The biggest number of {a} is {result}");
 }
 
+*/
+
+/*
+
+// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+
+int TwoDigtalNumber()
+{
+    int num1, num3;
+    int num = new Random().Next (100,1000);
+    Console.WriteLine (num);
+    
+    num1 = num / 100;
+    num3 = num % 10;
+
+    int result = num1*10 + num3;
+    return result;
+}
+
+Console.WriteLine (TwoDigtalNumber());
+
+*/
+
+/*
+// Задача: делиться ли второе число на первое без остатка. Если нет, то вывести остаток
+string IsAliquot(int num1, int num2)
+{
+    if (num2 % num1 == 0) //return "True"; При одном действии можно писать в строку
+    {
+        return "True";
+    }
+    else // return ("False, rest num is " + (num2 % num1)); При одном действии можно писать в строку
+    {
+        return ("False, rest num is " + (num2 % num1));
+    }
+}
+
+int a, b;
+
+Console.Write("Imput number a:");
+a = Convert.ToInt32 (Console.ReadLine());
+
+Console.Write("Imput number b:");
+b = Convert.ToInt32 (Console.ReadLine());
+
+Console.WriteLine (IsAliquot(a,b));
+
+
+/*
+
+//Задача: делиться ли число на 23 и на 7 одновременно без остатка. 
+
+
+bool IsAliquot2 (int num) // переменная bool - определяет значения true или fals
+{
+    // if (num % 23 == 0 && num % 7 == 0) return true;
+    // else return false;
+
+    return (num % 23 == 0 && num % 7 == 0);
+}
+
+if (0IsAliquot2 (10)) Console.WriteLine("число кратно");
+else Console.WriteLine("число yt кратно");
+*/
