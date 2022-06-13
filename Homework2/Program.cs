e6b5867ed645c01aa23505843b396a5b5a4f47bf
Valentin Burov <burov0742@gmail.com>
2022-06-13 15:01:32 +0300

    First and third exercise added

--- Homework2/Program.cs
+++ Homework2/Program.cs
@@ -1,2 +1,65 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+
+// Задача 1: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int FindSecondNum (int num)
+{
+    if (num > -999 && num < 999 &&  num != 0)
+    {
+    int des, SecondNum;
+
+    des  = num % 100;
+    SecondNum = des / 10;
+
+    return SecondNum;
+    }
+    else 
+
+    return -1;
+
+}
+
+int num,result;
+
+Console.WriteLine("Imput three-digital number: ");
+num = Convert.ToInt32(Console.ReadLine());
+
+result = FindSecondNum(num);
+
+if (result == -1) Console.WriteLine("Please imput correct number");
+else Console.WriteLine($"The second number is {result}");
+
+*/
+
+// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+/*
+Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+string Weekend(int day)
+{
+    if (day > 0 && day <= 7)
+    {
+        if (day == 6 || day == 7) return ("This is a weekend");
+        else return ("This is not weekend");
+        
+    }
+    else return ("Please, imput correct current day of week");
+}
+int day;
+Console.WriteLine("Imput current day of week: ");
+day = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Weekend(day));
