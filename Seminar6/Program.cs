b74730b70e29e3b9fa2e8e1944eaa609ec6713d2
Valentin <burov0742@gmail.com>
2022-06-28 14:02:43 +0400

    Seminar6 firs exercise added

--- Seminar6/Program.cs
+++ Seminar6/Program.cs
@@ -1,1 +1,31 @@
-﻿
+﻿/*
+int [] ReverseArray (int [] array)
+{
+    int temp;
+    int j = array.Length - 1;
+    int i = 0;
+
+    while (i < j)
+    {
+        temp = array[i];
+        array [i] = array [j];
+        array [j] = temp;
+
+        i++;
+        j++;
+    }
+    
+    return array;
+}
+
+int[] myArray = {2, 4, 6, 8};
+
+myArray = ReverseArray(myArray);
+
+for (int i = 0; i < myArray.Length; i++)
+{
+    Console.WriteLine(myArray + " ");
+}
+
+*/
+
