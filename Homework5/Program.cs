48139adfa624a9b2d5a16428a20843aac5de7466
Valentin <burov0742@gmail.com>
2022-06-28 19:20:43 +0400

    Third exercise  was added

--- Homework5/Program.cs
+++ Homework5/Program.cs
@@ -82,14 +82,18 @@ Console.WriteLine("Sum of all numbers with odd index is " + SumOddIndexNumbers(n
 //Задайте массив вещественных(дробных, см. Random().NextDouble()) чисел. Найдите разницу между максимальным и минимальным элементов массива.
 //[3 7 22 2 78] -> 76
 
-double[] CreateRandomDoubleArrray (int size)
+/*
+double[] CreateRandomDoubleArrray (int size, int minSize, int maxSize)
 {
     double [] randomArray = new double [size];
     for(int i = 0; i < size; i++)
     {
-        randomArray[i] = new Random().NextDouble()*100;
-        Console.Write(randomArray[i] + " ");
+        randomArray[i] = Math.Round(new Random().NextDouble()*(maxSize - minSize), 2);
+        Console.Write(randomArray[i] + " " + "|" + " ");
     }
+
+    Console.WriteLine();
+
     return randomArray;
 }
 
@@ -97,25 +101,48 @@ double[] CreateRandomDoubleArrray (int size)
 Console.Write("Imput size of array: ");
 int size = Convert.ToInt32(Console.ReadLine());
 
-double [] newArray = CreateRandomDoubleArrray (size);
+Console.Write("Imput min in array: ");
+int minSize = Convert.ToInt32(Console.ReadLine());
 
-double FindMaxNum(double[] array)
+Console.Write("Imput max in array: ");
+int maxSize = Convert.ToInt32(Console.ReadLine());
+
+double [] newArray = CreateRandomDoubleArrray (size, minSize, maxSize);
+
+double FindMaxNum (double [] array) // попытался сделать с циклом  for, но как-то криво получилось ((
 {
-    double max = 0;
-
-    for(int i = 0; i < array.Length; i++)
-
-    if (array[i] > array[i+1]) 
-    {
-        max = array[i];
-    }
-    else 
-    {
-        max = array[i+1];
-    }
+    int i = 0;
+    double max = array[i];
+    for (i = 0; i < array.Length-1; i++)
     
+    {
+        if (max < array[i+1]) max =  array[i+1];
+    }
+    Console.WriteLine();
+
     return max;
 }
 
-double maxNum = FindMaxNum(newArray);
-Console.WriteLine($"Max is {maxNum}");
+double FindMinNum (double [] array) //Здесь использовал цикл while
+{
+    int i = 0;
+    double min = array[i];
+
+    while (i < array.Length - 1)
+    {
+        if (min > array[i+1]) min = array [i+1];
+        i++;
+    }
+
+    Console.WriteLine();
+
+    return min;
+}
+
+Console.WriteLine("Max number in array is " + FindMaxNum(newArray));
+Console.WriteLine("Min number in array is " + FindMinNum(newArray));
+
+double Diff = FindMaxNum(newArray) - FindMinNum(newArray);
+Console.WriteLine($"Difference between max and min is {Diff}");
+Console.WriteLine();
+*/
